a9d8ef8744c053269a2297b28d965d352b290f1a
Daniel Adeleye <136744803+Tireni4560@users.noreply.github.com>
2024-07-19 00:05:49 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,22 +4,3 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );// Wait for page to load
-document.addEventListener('DOMContentLoaded', function() {
-
-  // Get reference to logo 
-  const logo = document.getElementById('logo');
-
-  // Display logo
-  logo.style.display = 'block';
-
-  // Wait 1 second before loading rest 
-  setTimeout(function() {
-
-    // Hide logo
-    logo.style.display = 'none';
-
-    // Rest of initialization code
-
-  }, 1000);
-
-});  
